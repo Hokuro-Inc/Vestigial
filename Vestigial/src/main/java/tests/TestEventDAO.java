9eaceeddc5044897030b778a6179f84ddd57b58f
ChrisLe7 <i82luesc@uco.es>
2021-11-13 11:26:10 +0100

    Subida de los test
    Se han creado los test para las pruebas de los DAO de Task y Event

---
+++ Vestigial/src/main/java/tests/TestEventDAO.java
@@ -0,0 +1,64 @@
+package tests;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+
+import es.uco.ism.business.event.EventDTO;
+import es.uco.ism.business.user.UserDTO;
+import es.uco.ism.data.EventDAO;
+import es.uco.ism.data.UserDAO;
+import es.uco.ism.utils.PasswordHashing;
+
+public class TestEventDAO {
+	public static void main (String[] args ) throws IOException {
+		Properties propertiesBD = new Properties();
+		FileReader fileReaderBD = new FileReader("WebContent/server.properties");
+		propertiesBD.load(fileReaderBD);
+
+		Properties propertiesSQL = new Properties();
+		FileReader fileReader = new FileReader("WebContent/sql.properties");
+		propertiesSQL.load(fileReader);
+		
+		System.out.println("Test EventDAO");
+		
+		EventDAO eventDAO = new EventDAO(propertiesBD.getProperty("URL"), propertiesBD.getProperty("USER"), propertiesBD.getProperty("PASSWORD"), propertiesSQL);
+
+		UserDAO userDAO = new UserDAO(propertiesBD.getProperty("URL"), propertiesBD.getProperty("USER"), propertiesBD.getProperty("PASSWORD"), propertiesSQL);
+		
+		String password = "myPassword";
+		String salt = PasswordHashing.createSalt();
+		String passwordHash = PasswordHashing.createHash(password, salt);
+				
+		UserDTO propietarioTest = new UserDTO("chris@gmail.com",passwordHash,salt,"660651903","123");
+		String ID = "12345"; // Como generamos las ID
+		EventDTO eventoTest = new EventDTO (ID,propietarioTest.getEmail(),new Date(), new Date(),"Test 1" , "Descripcion Prueba ");
+		
+		assert userDAO.Insert(propietarioTest) > 0 : "No se ha introducido el usuario de pruebas";
+		
+		assert eventDAO.Insert(eventoTest) > 0 : "No se ha introducido el evento de pruebas";
+		
+		EventDTO eventoBuscado = eventDAO.QueryById(eventoTest.getId());
+		assert eventoBuscado != null : "No se ha encontrado del evento";
+		
+		eventoTest.setDescription("Descripcion Actualizada");
+		
+		assert eventDAO.Update(eventoTest) > 0 : "No se ha introducido el evento de pruebas";
+		
+		eventoBuscado = eventDAO.QueryById(eventoTest.getId());
+		assert eventoBuscado != null : "No se ha encontrado del evento";
+		
+		assert eventoBuscado.getDescription().equals("Descripcion Actualizada") : "Error en el evento actualizado";
+
+		assert eventDAO.Delete(eventoTest.getId()) > 0 : "No se ha borrado el evento de pruebas";
+		
+		eventoBuscado = eventDAO.QueryById(eventoTest.getId());
+		assert eventoBuscado == null : "Se ha encontrado un evento borrado";
+		
+		assert userDAO.Delete(propietarioTest.getEmail()) > 0 : "No se ha borrado el usuario de pruebas";
+		
+		UserDTO user = userDAO.QueryByEmail(propietarioTest.getEmail());
+		assert user == null : "Se ha encontrado un usuario borrado";
+	}
+}
---
+++ Vestigial/src/main/java/tests/TestTaskDAO.java
@@ -0,0 +1,63 @@
+package tests;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import es.uco.ism.business.task.Status;
+import es.uco.ism.business.task.TaskDTO;
+import es.uco.ism.business.user.UserDTO;
+import es.uco.ism.data.TaskDAO;
+import es.uco.ism.data.UserDAO;
+import es.uco.ism.utils.PasswordHashing;
+
+public class TestTaskDAO {
+	public static void main (String[] args ) throws IOException {
+		Properties propertiesBD = new Properties();
+		FileReader fileReaderBD = new FileReader("WebContent/server.properties");
+		propertiesBD.load(fileReaderBD);
+
+		Properties propertiesSQL = new Properties();
+		FileReader fileReader = new FileReader("WebContent/sql.properties");
+		propertiesSQL.load(fileReader);
+		
+		System.out.println("Test TaskDAO");
+		
+		TaskDAO taskDAO = new TaskDAO(propertiesBD.getProperty("URL"), propertiesBD.getProperty("USER"), propertiesBD.getProperty("PASSWORD"), propertiesSQL);
+		UserDAO userDAO = new UserDAO(propertiesBD.getProperty("URL"), propertiesBD.getProperty("USER"), propertiesBD.getProperty("PASSWORD"), propertiesSQL);
+		
+		String password = "myPassword";
+		String salt = PasswordHashing.createSalt();
+		String passwordHash = PasswordHashing.createHash(password, salt);
+				
+		UserDTO propietarioTest = new UserDTO("chris@gmail.com",passwordHash,salt,"660651903","123");
+		String ID = "12345"; // Como generamos las ID
+		TaskDTO taskTest = new TaskDTO (ID,propietarioTest.getEmail(),"Test 1" , "Descripcion Prueba ",Status.InProcess);
+		
+		assert userDAO.Insert(propietarioTest) > 0 : "No se ha introducido el usuario de pruebas";
+		
+		assert taskDAO.Insert(taskTest) > 0 : "No se ha introducido la tarea de pruebas";
+		
+		TaskDTO taskBuscada = taskDAO.QueryById(taskTest.getId());
+		assert taskBuscada != null : "No se ha encontrado la tarea";
+		
+		taskTest.setDescription("Descripcion Actualizada");
+		
+		assert taskDAO.Update(taskTest) > 0 : "No se ha introducido la tarea de pruebas";
+		
+		taskBuscada = taskDAO.QueryById(taskTest.getId());
+		assert taskBuscada != null : "No se ha encontrado la tarea";
+		
+		assert taskBuscada.getDescription().equals("Descripcion Actualizada") : "Error en la tarea actualizado";
+
+		assert taskDAO.Delete(taskTest.getId()) > 0 : "No se ha borrado la tarea de pruebas";
+		
+		taskBuscada = taskDAO.QueryById(taskTest.getId());
+		assert taskBuscada == null : "Se ha encontrado una tarea borrada";
+		
+		assert userDAO.Delete(propietarioTest.getEmail()) > 0 : "No se ha borrado el usuario de pruebas";
+		
+		UserDTO user = userDAO.QueryByEmail(propietarioTest.getEmail());
+		assert user == null : "Se ha encontrado un usuario borrado";
+	}
+}
