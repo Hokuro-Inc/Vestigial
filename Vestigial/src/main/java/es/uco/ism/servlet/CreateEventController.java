5e28054d0dc487b6698b4296d890a9239b928366
ChrisLe7 <i82luesc@uco.es>
2021-11-13 23:32:27 +0100

    Creación de los primeros controladores para el calendario
    Se han creado los controladores para mostrar el calendario y para crear un evento

---
+++ Vestigial/src/main/java/es/uco/ism/servlet/CreateEventController.java
@@ -0,0 +1,104 @@
+package es.uco.ism.servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import es.uco.ism.business.event.EventDTO;
+import es.uco.ism.data.EventDAO;
+
+/**
+ * Servlet implementation class CreateEventController
+ */
+@WebServlet("/CreateEventController")
+public class CreateEventController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateEventController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String url_bd = request.getServletContext().getInitParameter("URL");
+		String username_bd = request.getServletContext().getInitParameter("USER");
+		String password_bd = request.getServletContext().getInitParameter("PASSWORD");
+		String sql = request.getServletContext().getInitParameter("sql");
+		
+		ServletContext application = getServletContext();
+		InputStream myIO = application.getResourceAsStream(sql);
+		java.util.Properties prop = new java.util.Properties();
+		prop.load(myIO);
+		
+		UserBean usuario = (UserBean) session.getAttribute("userBean");
+		
+		Boolean login = usuario != null && !usuario.getEmail().equals("");
+		
+		RequestDispatcher disparador = null;
+		
+		EventDAO eventDAO = new EventDAO(url_bd, username_bd, password_bd, prop);
+		String nextPage ="VISTA_MOSTRAR_CALENDARIO"; 
+		String mensajeNextPage = "";
+		
+		if (login) {
+			//Significa que me encuentro logueado, en dicho caso realizaremos las siguientes comprobaciones
+			
+			String nameEvent = request.getParameter("nameEvent");
+			if (nameEvent != null  && !nameEvent.equals("")) {
+				// Venimos de la vista por lo cual debemos de agregar el evento al usuario y regresarlo al controlador de calendario.
+				String descriptionEvent = request.getParameter("descriptionEvent");
+				
+				SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+				
+				SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+				
+				Date startEvent = inputFormat.parse(request.getParameter("startEvent"));
+				Date endEvent = outputFormat.parse(request.getParameter("endEvent"));
+				
+				String idEvent; // Generar ID evento
+				EventDTO newEvent = new EventDTO (idEvent, usuario.getEmail(), startEvent, endEvent, nameEvent, descriptionEvent);
+				if (eventDAO.Insert(newEvent) <=0 )  {
+					mensajeNextPage = "Ha surgido un problema a la hora de crear el evento";
+					nextPage = "CREAR_EVENTO";
+				}
+
+			}
+			else {
+				// Tenemos que dirigirnos a la vista
+				// No se si necesitamos enviarle algo a la vista de crear evento.
+				nextPage = "VISTA_CREAR_EVENTO";
+			}
+						
+		}
+		else{
+			// No se encuentra logueado, mandamos a la pagina de login.
+			nextPage = "LOGIN";
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Vestigial/src/main/java/es/uco/ism/servlet/ShowCalendarController.java
@@ -0,0 +1,87 @@
+package es.uco.ism.servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import es.uco.ism.data.EventDAO;
+
+/**
+ * Servlet implementation class ShowCalendar
+ */
+@WebServlet("/ShowCalendar")
+public class ShowCalendarController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowCalendarController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		//Obtenemos los parametros para poder realizar las conexiones con la base de datos
+		HttpSession session = request.getSession();
+		String url_bd = request.getServletContext().getInitParameter("URL");
+		String username_bd = request.getServletContext().getInitParameter("USER");
+		String password_bd = request.getServletContext().getInitParameter("PASSWORD");
+		String sql = request.getServletContext().getInitParameter("sql");
+		
+		ServletContext application = getServletContext();
+		InputStream myIO = application.getResourceAsStream(sql);
+		java.util.Properties prop = new java.util.Properties();
+		prop.load(myIO);
+		
+		UserBean usuario = (UserBean) session.getAttribute("userBean");
+		
+		Boolean login = usuario != null && !usuario.getEmail().equals("");
+		
+		RequestDispatcher disparador = null;
+		
+		EventDAO eventDAO = new EventDAO(url_bd, username_bd, password_bd, prop);
+		String nextPage ="VISTA_MOSTRAR_CALENDARIO"; 
+		String mensajeNextPage = "";
+		
+		if (login) {
+			//Significa que me encuentro logueado, en dicho caso realizaremos las siguientes comprobaciones
+			
+			//Supongo que solicitaremos todos los eventos del usuario para mostrar no?
+			ArrayList <EventDTO> listadoEventos = eventDAO.QueryByEmail(usuario.getEmail());
+			
+			CalendarioBeans calendarioUsuario = new CalendarioBeans ();
+			
+			calendarioUsuario.setEventos (listadoEventos);
+			
+			session.setAttribute("Calendario", calendarioUsuario);
+			
+		}
+		else{
+			// No se encuentra logueado, mandamos a la pagina de login.
+			nextPage = "LOGIN";
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
