aa81f62e97065bc10962ae9cc637d3ed4da1a7f8
Pedro Pablo García Pozo <i82gapop@uco.es>
2021-11-20 18:45:39 +0100

    Partially added Mail functionality

--- Vestigial/src/main/java/tests/TestMail.java
+++ Vestigial/src/main/java/tests/TestMail.java
@@ -1,41 +1,99 @@
 package tests;
 
-import es.uco.ism.utils.Mail;
-
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class TestMail {
+
+    // Replace sender@example.com with your "From" address.
+    // This address must be verified.
+    static final String FROM = "hokuroincorporated@gmail.com";
+    static final String FROMNAME = "Hokuro";
 	
-	private static void enviarConGMail(String destinatario, String asunto, String cuerpo) {
-	    // Esto es lo que va delante de @gmail.com en tu cuenta de correo. Es el remitente también.
-	    private String remitente = "nomcuenta";  //Para la dirección nomcuenta@gmail.com
+    // Replace recipient@example.com with a "To" address. If your account 
+    // is still in the sandbox, this address must be verified.
+    static final String TO = "i82gapop@uco.es";
+    
+    // Replace smtp_username with your Amazon SES SMTP user name.
+    static final String SMTP_USERNAME = "hokuroincorporated@gmail.com";
+    
+    // Replace smtp_password with your Amazon SES SMTP password.
+    static final String SMTP_PASSWORD = "adaylachupa";
+    
+    // The name of the Configuration Set to use for this message.
+    // If you comment out or remove this variable, you will also need to
+    // comment out or remove the header below.
+    static final String CONFIGSET = "ConfigSet";
+    
+    // Amazon SES SMTP host name. This example uses the US West (Oregon) region.
+    // See https://docs.aws.amazon.com/ses/latest/DeveloperGuide/regions.html#region-endpoints
+    // for more information.
+    static final String HOST = "smtp.gmail.com";
+    
+    // The port you will connect to on the Amazon SES SMTP endpoint. 
+    static final int PORT = 587;
+    
+    static final String SUBJECT = "Vestigial test (SMTP interface accessed using Java)";
+    
+    static final String BODY = String.join(
+    	    System.getProperty("line.separator"),
+    	    "<h1>HOKURO EMAIL TEST</h1>",
+    	    "<p>This email was sent with Vestigial using the ", 
+    	    "<a href='https://github.com/javaee/javamail'>Javamail Package</a>",
+    	    " for <a href='https://www.java.com'>Java</a>."
+    	);
 
-	    Properties props = System.getProperties();
-	    props.put("mail.smtp.host", "smtp.gmail.com");  //El servidor SMTP de Google
-	    props.put("mail.smtp.user", remitente);
-	    props.put("mail.smtp.clave", "miClaveDeGMail");    //La clave de la cuenta
-	    props.put("mail.smtp.auth", "true");    //Usar autenticación mediante usuario y clave
-	    props.put("mail.smtp.starttls.enable", "true"); //Para conectar de manera segura al servidor SMTP
-	    props.put("mail.smtp.port", "587"); //El puerto SMTP seguro de Google
+    public static void main(String[] args) throws Exception {
 
-	    Session session = Session.getDefaultInstance(props);
-	    MimeMessage message = new MimeMessage(session);
+        // Create a Properties object to contain connection configuration information.
+    	Properties props = System.getProperties();
+    	props.put("mail.transport.protocol", "smtp");
+    	props.put("mail.smtp.port", PORT); 
+    	props.put("mail.smtp.starttls.enable", "true");
+    	props.put("mail.smtp.auth", "true");
 
-	    try {
-	        message.setFrom(new InternetAddress(remitente));
-	        message.addRecipient(Message.RecipientType.TO, destinatario);   //Se podrían añadir varios de la misma manera
-	        message.setSubject(asunto);
-	        message.setText(cuerpo);
-	        Transport transport = session.getTransport("smtp");
-	        transport.connect("smtp.gmail.com", remitente, clave);
-	        transport.sendMessage(message, message.getAllRecipients());
-	        transport.close();
-	    }
-	    catch (MessagingException me) {
-	        me.printStackTrace();   //Si se produce un error
-	    }
-	}
-	}
+        // Create a Session object to represent a mail session with the specified properties. 
+    	Session session = Session.getDefaultInstance(props);
+
+        // Create a message with the specified information. 
+        MimeMessage msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(FROM,FROMNAME));
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(TO));
+        msg.setSubject(SUBJECT);
+        msg.setContent(BODY,"text/html");
+        
+        // Add a configuration set header. Comment or delete the 
+        // next line if you are not using a configuration set
+        msg.setHeader("X-SES-CONFIGURATION-SET", CONFIGSET);
+            
+        // Create a transport.
+        Transport transport = session.getTransport();
+                    
+        // Send the message.
+        try
+        {
+            System.out.println("Sending...");
+            
+            // Connect to Amazon SES using the SMTP username and password you specified above.
+            transport.connect(HOST, SMTP_USERNAME, SMTP_PASSWORD);
+        	
+            // Send the email.
+            transport.sendMessage(msg, msg.getAllRecipients());
+            System.out.println("Email sent!");
+        }
+        catch (Exception ex) {
+            System.out.println("The email was not sent.");
+            System.out.println("Error message: " + ex.getMessage());
+        }
+        finally
+        {
+            // Close and terminate the connection.
+            transport.close();
+        }
+    }
 }
