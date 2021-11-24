310825b417473de4cfb38fa8a58c1038724fa006
Lepre lepreeeee <i82bocar@uco.es>
2021-11-24 17:40:27 +0100

    Beans terminados

--- Vestigial/src/main/java/es/uco/ism/display/TaskBean.java
+++ Vestigial/src/main/java/es/uco/ism/display/TaskBean.java
@@ -1,5 +1,32 @@
 package es.uco.ism.display;
 
-public class TaskBean {
+import java.io.Serializable;
+
+import es.uco.ism.business.task.TaskDTO;
+
+public class TaskBean implements Serializable{
     
+    private static final long serialVersionUID = 1L;
+
+    private String id = "";
+    private TaskDTO task = null;
+
+
+    public String getID(){
+        return id;
+    }
+    
+    public void setID (String id){
+        this.id = id;
+    }
+
+    public TaskDTO getTask(){
+        return task;
+    }
+
+    public void setTask (TaskDTO task){
+        this.task = task;
+    }
+
+
 }
--- Vestigial/src/main/java/es/uco/ism/display/ToDoListBean.java
+++ Vestigial/src/main/java/es/uco/ism/display/ToDoListBean.java
@@ -1,5 +1,30 @@
 package es.uco.ism.display;
 
-public class ToDoListBean {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import es.uco.ism.business.task.TaskDTO;
+
+public class ToDoListBean implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private ArrayList <TaskDTO> listatareas = null;
+
+    public ArrayList <TaskDTO> getToDoList(){
+        return listatareas;
+    }
+    
+    public void setToDoList (ArrayList <TaskDTO> listatareas){
+        this.listatareas = listatareas;
+    }
+
+    public TaskDTO get(int index){
+        return this.listatareas.get(index);
+    }
+
+    public void set (int index, TaskDTO task){
+        this.listatareas.set(index, task);
+    }
     
 }
