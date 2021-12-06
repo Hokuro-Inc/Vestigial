bf99128dc6d80683cca798378cbfb62a6e302396
ChrisLe7 <i82luesc@uco.es>
2021-12-06 17:50:24 +0100

    Agregados los direccionamientos finales

--- IonicAppVestigial/src/app/services/contacts-service/contacts.service.ts
+++ IonicAppVestigial/src/app/services/contacts-service/contacts.service.ts
@@ -9,19 +9,23 @@ export class ContactsService {
   constructor(private httpClient: HttpClient) { }
 
   public getData(data: string) {
-    return this.httpClient.post('http://localhost:8080/Vestigial/ShowAgenda', data, { responseType:'text' });
+//    return this.httpClient.post('https://hokuro.xyz/Vestigial/ShowAgenda', data, { responseType:'text' });
+    return this.httpClient.post('https://localhost::8080/Vestigial/ShowAgenda', data, { responseType:'text' });
   }
 
   public addContact(data: string) {
-    return this.httpClient.post('http://localhost:8080/Vestigial/AddContact', data, { responseType:'text' });
+ //   return this.httpClient.post('https://hokuro.xyz/Vestigial/AddContact', data, { responseType:'text' });
+   return this.httpClient.post('https://localhost::8080/Vestigial/AddContact', data, { responseType:'text' });
   }
 
   public updateContact(data: string) {
-    return this.httpClient.post('http://localhost:8080/Vestigial/UpdateContact', data, { responseType:'text' });
+    //return this.httpClient.post('https://hokuro.xyz/Vestigial/UpdateContact', data, { responseType:'text' });
+    return this.httpClient.post('https://localhost::8080/Vestigial/UpdateContact', data, { responseType:'text' });
   }
 
   public removeContact(data: string) {
-    return this.httpClient.post('http://localhost:8080/Vestigial/RemoveContact', data, { responseType:'text' });
+//    return this.httpClient.post('https://hokuro.xyz/Vestigial/RemoveContact', data, { responseType:'text' });
+    return this.httpClient.post('https://localhost::8080/Vestigial/RemoveContact', data, { responseType:'text' });
   }
   
 }
